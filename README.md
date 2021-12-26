d7c243e130d995c56ec69d5d482416aa3516b7c4
Rodion Zainullin <rodion-zainullin@mail.ru>
2021-12-26 18:30:22 +0700

    all updates

--- README.md
+++ README.md
@@ -5,5 +5,8 @@ Maintainability Badge
 
 <a href="https://codeclimate.com/github/Rodion94/frontend-project-lvl2/maintainability"><img src="https://api.codeclimate.com/v1/badges/b14662b828f5193e0d36/maintainability" /></a>
 
-Comparison #1
+Comparison №1
   https://asciinema.org/a/a5KrJSJB0sbGojWTrj8GRtBcc
+
+  Comparison №2
+   https://asciinema.org/a/0qpF3GFWkBSWLgOSIgkjan3Lw
--- bin/gendiff.js
+++ bin/gendiff.js
@@ -1,20 +1,16 @@
 #!/usr/bin/env node
 
-import { Command } from 'commander/esm.mjs';
-import getComprasion from '../src/index.js';
-const program = new Command();
+import Command from 'commander';
+import genDiff from '../index.js';
 
-program.option('-V, --version', 'output the version number');
-program.description(
-  ' Compares two configuration files and shows a difference.',
-);
-program.option('-f, --format [type]', 'output format');
-program.arguments('<filepath1> <filepath2>');
-program.action((filepath1, filepath2) => {
-  const result = getComprasion(filepath1, filepath2);
-  console.log(result);
-  return result;
-});
+const program = Command;
+
+program
+  .version('0.1.0')
+  .arguments('<filepath1> <filepath2>')
+  .description('Compares two configuration files and shows a difference.')
+  .option('-f, --format [type]', 'output format', 'stylish')
+  .action((filepath1, filepath2) => {
+    console.log(genDiff(filepath1, filepath2, program.opts().format));
+  });
 program.parse(process.argv);
-  
-export default program;
